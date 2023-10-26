d63de58732755ac13def261ba41bbe106a8a007d
Nadezhda Lyovushkina <lyovushkina.nv@gmail.com>
2023-10-26 22:00:21 +0300

    HW6 try/except IndexError

--- src/video.py
+++ src/video.py
@@ -7,16 +7,27 @@ api_key: str = os.getenv('YOUTUBE_API_KEY')  # YOUTUBE_API_KEY скопиров�
 
 youtube = build('youtube', 'v3', developerKey=api_key)  # создать специальный объект для работы с API
 
+
 class Video:
     """Класс для ютуб_видео"""
 
     def __init__(self, video_id):
-        self.video_id = video_id
-        self.video_info = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',id=video_id).execute()
-        self.title = self.video_info['items'][0]['snippet']['title']
-        self.url = 'https://youtu.be/' + self.video_id
-        self.view_count = self.video_info['items'][0]['statistics']['viewCount']
-        self.like_count = self.video_info['items'][0]['statistics']['likeCount']
+
+            self.video_id = video_id
+            self.video_info = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',id=video_id).execute()
+
+            try:
+                self.title = self.video_info['items'][0]['snippet']['title']
+                self.url = 'https://youtu.be/' + self.video_id
+                self.view_count = self.video_info['items'][0]['statistics']['viewCount']
+                self.like_count = self.video_info['items'][0]['statistics']['likeCount']
+
+            except IndexError:
+                self.title = None
+                self.url = None
+                self.view_count = None
+                self.like_count = None
+
 
     def __str__(self):
         return f"{self.title}"
